efb6b9d896fbfd5370d6356da23299ba13983ac4
duongpk <duongpk@gmail.com>
2025-06-25 15:01:56 +0700

    commit_25-06-2025_15-01-56

--- ansible_deploy_channel/backup_local.sh
+++ ansible_deploy_channel/backup_local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BACKUP_DIR=/home/ansible/ansible/ansible_deploy_channel/bk
-SRC_DIR=/home/ansible/ansible/ansible_deploy_channel
+SRC_DIR=/home/ansible/ansible/ansible_deploy_channel/roles/deploy_file/templates
 TIMESTAMP=$(date +"%d%m%Y_%H%M%S")
-DEST_DIR="$BACKUP_DIR/$TIMESTAMP"
+DEST_DIR="$BACKUP_DIR/deploy_file_$TIMESTAMP"
 
 mkdir -p "$DEST_DIR"
 cp $SRC_DIR/* "$DEST_DIR" 2>/dev/null || true
